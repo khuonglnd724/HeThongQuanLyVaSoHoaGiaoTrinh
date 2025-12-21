a83e953622d75d1e4e1bdd5653fcc636f4a21f55
khuonglnd724 <khuonglnd7241@ut.edu.vn>
2025-12-21 16:37:14 +0700

    Initial Spring Boot project

---
+++ src/main/java/com/smd/ht_giaotrinhsohoa/HtGiaotrinhsohoaApplication.java
@@ -0,0 +1,13 @@
+package com.smd.ht_giaotrinhsohoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtGiaotrinhsohoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtGiaotrinhsohoaApplication.class, args);
+	}
+
+}
