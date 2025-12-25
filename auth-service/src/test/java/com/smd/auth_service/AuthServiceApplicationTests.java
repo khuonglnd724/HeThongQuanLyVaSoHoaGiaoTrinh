a2216cbae67e2b7b1daf1218974604d571f18141
khuonglnd724 <khuonglnd7241@ut.edu.vn>
2025-12-25 21:19:59 +0700

    Disable SQL script initialization in tests

--- auth-service/src/test/java/com/smd/auth_service/AuthServiceApplicationTests.java
+++ auth-service/src/test/java/com/smd/auth_service/AuthServiceApplicationTests.java
@@ -10,7 +10,8 @@ import org.springframework.test.context.TestPropertySource;
 @TestPropertySource(properties = {
 	"spring.datasource.url=jdbc:h2:mem:testdb",
 	"spring.datasource.driver-class-name=org.h2.Driver",
-	"spring.jpa.hibernate.ddl-auto=create-drop"
+	"spring.jpa.hibernate.ddl-auto=create-drop",
+	"spring.sql.init.mode=never"
 })
 class AuthServiceApplicationTests {
 
