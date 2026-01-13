04f68d7217b7c6682313a65806ec6eb9b9ee8aeb
TuyetLoan02 <nguyenvutuyetloan@gmail.com>
2026-01-13 22:47:07 +0700

    Add WorkflowApprovalEvent DTO

---
+++ backend/workflow-service/src/main/java/com/smd/workflow_service/event/WorkflowApprovalEvent.java
@@ -0,0 +1,44 @@
+package com.smd.workflow_service.event;
+
+import com.smd.workflow_service.domain.WorkflowState;
+import java.util.UUID;
+
+public class WorkflowApprovalEvent {
+
+    private UUID workflowId;
+    private WorkflowState fromState;
+    private WorkflowState toState;
+    private String actionBy;
+
+    public UUID getWorkflowId() {
+        return workflowId;
+    }
+
+    public void setWorkflowId(UUID workflowId) {
+        this.workflowId = workflowId;
+    }
+
+    public WorkflowState getFromState() {
+        return fromState;
+    }
+
+    public void setFromState(WorkflowState fromState) {
+        this.fromState = fromState;
+    }
+
+    public WorkflowState getToState() {
+        return toState;
+    }
+
+    public void setToState(WorkflowState toState) {
+        this.toState = toState;
+    }
+
+    public String getActionBy() {
+        return actionBy;
+    }
+
+    public void setActionBy(String actionBy) {
+        this.actionBy = actionBy;
+    }
+}
